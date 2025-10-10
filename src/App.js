ff7951f4ef52088dc21181a63ca420c42ea3b8bb
Elias Ghanayem <eejaypersonal@gmail.com>
2025-10-10 16:34:00 -0500

    Add Google Analytics 4 tracking to App.js using react-ga4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import CustomNavbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,22 +11,38 @@ import Gallery from './pages/Gallery';
 import SocialBlog from './pages/SocialBlog';
 import Contact from './pages/Contact';
 
+// Initialize Google Analytics once (outside component)
+ReactGA.initialize('G-HJ89F9Q3VF');
+
+// Create a wrapper to track route changes
+function GAListener({ children }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+
+  return children;
+}
+
 function App() {
   return (
     <Router>
-      <CustomNavbar />
-      <div className="container my-4" style={{ minHeight: '80vh' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/bridal" element={<Bridal />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/social-blog" element={<SocialBlog />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-      <Footer />
+      <GAListener>
+        <CustomNavbar />
+        <div className="container my-4" style={{ minHeight: '80vh' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/bridal" element={<Bridal />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/social-blog" element={<SocialBlog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </GAListener>
     </Router>
   );
 }
