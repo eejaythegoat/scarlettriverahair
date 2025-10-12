9319bf1921262bb3f66f2383671284ff88f7c15d
Elias Ghanayem <eejaypersonal@gmail.com>
2025-10-11 23:01:34 -0500

    Fix: use correct react-icons import, update install steps, and polish UI as per TailwindCSS style guidelines

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { FaInstagram, FaTiktok, FaFacebook, FaLinkedin } from "react-icons/fa6";
+import { FaInstagram, FaTiktok, FaFacebook, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
